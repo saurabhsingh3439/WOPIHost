03651ddbbf11265f7c63c65f747eb5a597ea6f0e
DishantVashistha <dishant.vashistha@accolite.com>
2021-02-12 16:21:43 +0530

    Copyright Details added in each file

--- MS-WOPI/Interfaces/IStatusValidator.cs
+++ MS-WOPI/Interfaces/IStatusValidator.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+Copyright Mitratech Holdings Inc, 2021
+This software is provided under the terms of a License Agreement and may
+only be used and/or copied in accordance with the terms of such agreement.
+Neither this software nor any copy thereof may be provided or otherwise
+made available to any other person. No title or ownership of this software
+is hereby transferred.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using MS_WOPI.Response;
